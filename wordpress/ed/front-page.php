b25f5a75a11b8ff7d4f475537115520bea840e1f
vivallo <86535902+vivallo@users.noreply.github.com>
2024-05-25 00:01:34 -0400

    Actualizado

--- wordpress/ed/front-page.php
+++ wordpress/ed/front-page.php
@@ -9,14 +9,20 @@
 
 <?php if($opciones['ordenSecciones'] == 'ordenUno' ) {  
  get_template_part( 'template-parts/cuerpo', 'servicio' );
- get_template_part('category', 'productos');
-} else {
- get_template_part('category', 'productos');
+ get_template_part( 'template-parts/cuerpo', 'producto' );
+} else {  
+ get_template_part( 'template-parts/cuerpo', 'producto' );
  get_template_part( 'template-parts/cuerpo', 'servicio' );
 } ?>
 
 <?php get_template_part( 'template-parts/cuerpo', 'somos' ); ?>
-<?php get_template_part( 'template-parts/cuerpo', 'cliente' ); ?>
+
+<?php
+  $opcionV = get_option('OpcionCyRclientes');
+  if($opcionV['clienteVision'] == 'mostrarCliente') {
+	get_template_part( 'template-parts/cuerpo', 'cliente' );
+  }
+?>
 <?php get_template_part( 'template-parts/cuerpo', 'contacto' ); ?>
 <?php get_template_part( 'template-parts/cuerpo', 'mapa' ); ?>
 <?php get_footer('principal'); ?>
