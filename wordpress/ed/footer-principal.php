afcdd9f638b249642965ffb9b50c2162f26b803e
vivallo <86535902+vivallo@users.noreply.github.com>
2024-05-24 23:50:31 -0400

    Actualizado

--- wordpress/ed/footer-principal.php
+++ wordpress/ed/footer-principal.php
@@ -8,37 +8,24 @@
             <div class="col-12 buscanos">
               <h2 class="pt-3 pb-3">Búscanos</h2>
               <ul class="w-100 d-flex flex-wrap text-left p-0">
-              <?php $iconos = cmb2_get_option('OpcionCyRubicacion', 'rSociales');
-               $cont = 1;
-               foreach($iconos as $icono) {
-               if( $cont >= 2 ) { echo '<li class="mr-3">|</li>'; } ?>
-                <li class="mr-3"><a href="<?php echo $icono['direccionURL'];?>" target="_blank"><i class="icon-cyr-<?php echo $icono['redSocial'];?>"></i> <?php echo $icono['redSocial'];?></a></li>
-                <?php $cont++; } ?>
+              <?php $ubicar = get_option('OpcionCyRubicacion'); ?>
+                <?php if(!$ubicar['facebook']=='') { ?><li class="mr-3"><a href="<?php echo $ubicar['facebook'];?>" target="_blank"><i class="icon-cyr-facebook"></i> Facebook </a></li><?php } ?>
+                <?php if(!$ubicar['instagram']=='') { ?><li class="mr-3"><a href="https://www.instagram.com/<?php echo $ubicar['instagram'];?>/" target="_blank"><i class="icon-cyr-instagram"></i> Instagram </a></li><?php } ?>
+                <?php //echo '<li class="mr-3">|</li>'; ?>
               </ul>
             </div>
             <div id="textoDos" class="col-12 buscanos">
              <h2 class="pt-3 pb-3">Contacto</h2>
               <ul class="w-100 text-left p-0">
-               <?php $fijos = cmb2_get_option('OpcionCyRubicacion', 'listaFonoFijo'); ?>
-               <?php $fonos = cmb2_get_option('OpcionCyRubicacion', 'listaFonos'); ?>
-               <?php $whatsapps = cmb2_get_option('OpcionCyRubicacion', 'numeroWA'); ?>
-               <?php $correos = cmb2_get_option('OpcionCyRubicacion', 'listaCorreos'); ?>
-               <?php $direcciones = cmb2_get_option('OpcionCyRubicacion', 'listaDirecciones'); ?>
+                
                
-               <?php if(!$fijos == '') { ?>
-               <li><i class="fa fa-phone-volume"></i> <span>Fono Fijo:</span> <span id="txtEfecto4"><?php $contFF = 1 ; foreach($fijos as $fijo) { if( $contFF >= 2 ) { echo ' - ' ; }; echo $fijo; $contFF++; } ?></span></li><?php } ?>
+                <li><i class="icon-cyr-movil"></i> <span>Fono Movil:</span> <span id="txtEfecto4"><?php echo $ubicar['movilUno']; ?></span></li>
                 
-                <?php if(!$fonos == '') { ?>
-                <li><i class="icon-cyr-movil"></i> <span>Fono Celular:</span> <span id="txtEfecto4"><?php $contF = 1 ; foreach($fonos as $fono) { if( $contF >= 2 ) { echo ' - ' ; }; echo $fono; $contF++; } ?></span></li><?php } ?>
+                <li><i class="icon-cyr-whatsapp"></i> <span>WhatsApp:</span> <span id="txtEfecto5"><?php echo $ubicar['WaUno']; ?></span></li>
+               
+                <li><i class="icon-cyr-correo"></i> <span>Correo:</span>  <span id="txtEfecto6"><?php echo $ubicar['correoUno']; ?></span></li>
                 
-                <?php if(!$whatsapps == '') { ?>
-                <li><i class="icon-cyr-whatsapp"></i> <span>WhatsApp:</span> <span id="txtEfecto5"><?php $contWA = 1 ; foreach($whatsapps as $whatsapp) { if( $contWA >= 2 ) { echo ' - ' ; }; echo '(+56) '.$whatsapp; $contWA++; } ?></span></li><?php } ?>
-                
-                <?php if(!$correos == '') { ?>
-                <li><i class="icon-cyr-correo"></i> <span>Correo:</span>  <span id="txtEfecto6"><?php $contC = 1 ; foreach($correos as $correo) { if( $contC >= 2 ) { echo ' - ' ; }; echo $correo; $contC++; } ?></span></li><?php } ?>
-                
-                <?php if(!$direcciones == ''){ ?>
-                <li><i class="icon-cyr-direccion"></i> <span>Dirección:</span> <span id="txtEfecto7"><?php $contD = 1 ; foreach($direcciones as $direccion) { if( $contD >= 2 ) { echo ' - ' ; }; echo $direccion; $contD++;} ?></span></li><?php } ?>
+                <li><i class="icon-cyr-direccion"></i> <span>Dirección:</span> <span id="txtEfecto7"><?php echo $ubicar['direccionUno']; ?></span></li>
               </ul>
             </div>
           </div>
@@ -47,22 +34,26 @@
         <div class="col-4 secciones seccion">
           <h2 class="pt-3 pb-3">Secciones</h2>
           <ul class="w-100 text-left p-0">
-            <li class="marca wow fadeInUp"><a href="#inicio"><i class="fa fa-angle-right"></i> Inicio</a></li>
-            <li class="marca wow fadeInUp" data-wow-delay="0.2s"><a href="#servicios"><i class="fa fa-angle-right"></i> Servicios</a></li>
-            <li class="marca wow fadeInUp" data-wow-delay="0.3s"><a href="#productos"><i class="fa fa-angle-right"></i> Productos</a></li>
-            <li class="marca wow fadeInUp" data-wow-delay="0.4s"><a href="#somos"><i class="fa fa-angle-right"></i> Somos</a></li>
-            <li class="marca wow fadeInUp" data-wow-delay="0.5s"><a href="#clientes"><i class="fa fa-angle-right"></i> Clientes</a></li>
-            <li class="marca wow fadeInUp" data-wow-delay="0.6s"><a href="#contacto"><i class="fa fa-angle-right"></i> Contacto</a></li>
+            <li class="marca wow fadeInUp"><a href="#inicio"><i class="icon-cyr-flecha-der"></i> Inicio</a></li>
+            <li class="marca wow fadeInUp" data-wow-delay="0.2s"><a href="#servicios"><i class="icon-cyr-flecha-der"></i> Servicios</a></li>
+            <li class="marca wow fadeInUp" data-wow-delay="0.3s"><a href="#productos"><i class="icon-cyr-flecha-der"></i> Productos</a></li>
+            <li class="marca wow fadeInUp" data-wow-delay="0.4s"><a href="#somos"><i class="icon-cyr-flecha-der"></i> Somos</a></li>
+             <?php
+              $opcionV = get_option('OpcionCyRclientes');
+                if($opcionV['clienteVision'] == 'mostrarCliente') {
+            	  echo '<li class="marca wow fadeInUp" data-wow-delay="0.5s"><a href="#clientes"><i class="icon-cyr-flecha-der"></i> Clientes</a></li>';
+				}
+			?>
+            <li class="marca wow fadeInUp" data-wow-delay="0.6s"><a href="#contacto"><i class="icon-cyr-flecha-der"></i> Contacto</a></li>
           </ul>
         </div>
                 
       </div>
       
-      <?php $imgLogo2 = get_option('OpcionCyRgeneral'); ?>
       <div class="row final">
-        <div class="col-4 text-center texto align-self-center"> Copyright 2018 &copy; | <b>Castro y Rivera</b> extintores</div>
-        <div class="col-4 text-center centro"><img src="<?php echo $imgLogo2['imgLogo2']; ?>" alt=""></div>
-        <div class="col-4 externo">Este sitio es impulsado por:<a href="https://webgrafica.cl" target="blanc"><img src="<?php echo get_theme_file_uri( 'img/isotipoWG.png'); ?>" alt=""></a></div>
+        <div class="col-12 col-lg-4 text-center texto align-self-center py-5 p-lg-0"> Copyright 2020 &copy; | <b>Extintores a Domicilio</b></div>
+        <div class="col-12 col-lg-4 text-center centro"><img src="<?php echo get_theme_file_uri( 'img/logo-pie.png'); ?>" alt=""></div>
+        <div class="col-12 col-lg-4 text-center externo">Sitio impulsado por:<br/><a href="https://webgrafica.cl" target="blanc"><img src="<?php echo get_theme_file_uri( 'img/logoWG.png'); ?>" alt=""></a></div>
       </div>
       
     </div>
